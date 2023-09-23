822bedb624e73cb688f51bb63e0f075a2579f54b
mahdi12ad <exxzam@gmail.com>
2023-09-23 10:46:20 +0330

    Login and register pages with needed utils functions for phase 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import Login from "./features/login";
+import { Route, Routes } from "react-router-dom";
+import Register from "./features/register";
 
-function App() {
-  return <div id="root"></div>;
-}
+const App = () => {
+    return (
+        <div className="flex flex-col h-[100vh] bg-blue-secondary">
+            <div className="flex flex-grow items-center justify-center z-10">
+                <Routes>
+                    TODO <Route path="/" element={<h1>Main Page</h1>} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </div>
+        </div>
+    );
+};
 
 export default App;
