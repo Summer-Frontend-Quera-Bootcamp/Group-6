8bf8878b4ced3abd701e81c13e29ebc92f477b47
mahdi12ad <exxzam@gmail.com>
2023-09-26 10:33:00 +0330

    added new routes and redirects

--- src/pages/pagesData.tsx
+++ src/pages/pagesData.tsx
@@ -1,13 +1,20 @@
-import { Forgot, Login, Register, ResetPassword } from "../features";
+import {
+    Forgot,
+    Login,
+    Register,
+    ResetPassword,
+    ProfileSettings,
+} from "../features";
 import AuthLayout from "../layouts/authentication";
 import ProfileLayout from "../layouts/profile";
 import { routerType } from "../types/router.types";
+import { Navigate } from "react-router-dom";
 
 const AuthPagesData: routerType[] = [
     {
         path: "/",
         title: "login",
-        element: <AuthLayout children={<Login />} />,
+        element: <Navigate to="/login" />,
     },
     {
         path: "/login",
@@ -35,7 +42,12 @@ const ProfilePagesData: routerType[] = [
     {
         path: "/profile",
         title: "profile",
-        element: <ProfileLayout children={<h1>profile</h1>} />,
+        element: <ProfileLayout children={<ProfileSettings />} />,
+    },
+    {
+        path: "/profile/settings",
+        title: "settings",
+        element: <ProfileLayout children={<ProfileSettings />} />,
     },
     {
         path: "/profile/details",
