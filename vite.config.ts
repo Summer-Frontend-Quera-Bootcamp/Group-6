af02be4d4f936f89b44d8e717aedf5f67f0246fc
mahdi12ad <exxzam@gmail.com>
2023-09-27 11:58:29 +0330

    added aliases configuration to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+            "@context": path.resolve(__dirname, "./src/context"),
+            "@components": path.resolve(__dirname, "./src/components"),
+            "@assets": path.resolve(__dirname, "./src/assets"),
+            "@hooks": path.resolve(__dirname, "./src/hooks"),
+            "@layouts": path.resolve(__dirname, "./src/layouts"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@types": path.resolve(__dirname, "./src/types"),
+            "@utils": path.resolve(__dirname, "./src/utils"),
+            "@features": path.resolve(__dirname, "./src/features"),
+        },
+    },
+    plugins: [react()],
+});
