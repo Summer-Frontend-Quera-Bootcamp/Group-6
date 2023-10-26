dcb9cc52991c54f323e56f3d8a2ffef5de233bab
Amir mohammad Biedaar <amir.m.biedaar@gmail.com>
2023-10-26 13:48:33 +0330

    fix(filters): show selected items

--- src/components/filters/Filter/FilterRow.tsx
+++ src/components/filters/Filter/FilterRow.tsx
@@ -5,63 +5,66 @@ import { IFilterData } from "@/context/FilterContext";
 import DeleteIcon from "../assets/icons/DeleteIcon";
 
 const whereList = [
-    { id: 1, title: "تاریخ" },
-    { id: 2, title: "تگ" },
-    { id: 3, title: "اعضا" },
-    { id: 4, title: "اولویت" },
+  { id: 1, title: "تاریخ" },
+  { id: 2, title: "تگ" },
+  { id: 3, title: "اعضا" },
+  { id: 4, title: "اولویت" },
 ];
 
 const tagList: { id: number; title: ITagTitle }[] = [
-    { id: 1, title: "درس" },
-    { id: 2, title: "پروژه" },
-    { id: 3, title: "کار" },
+  { id: 1, title: "درس" },
+  { id: 2, title: "پروژه" },
+  { id: 3, title: "کار" },
 ];
 
 const statusList = [
-    { id: 1, title: "است" },
-    { id: 2, title: "نیست" },
+  { id: 1, title: "است" },
+  { id: 2, title: "نیست" },
 ];
 
 interface IFilterRowProps {
-    filterData: IFilterData;
-    onUpdate: (item: IFilterData) => void;
-    onDelete: (id: number) => void;
+  filterData: IFilterData;
+  onUpdate: (item: IFilterData) => void;
+  onDelete: (id: number) => void;
 }
 
 const FilterRow: FC<IFilterRowProps> = ({ filterData, onUpdate, onDelete }) => {
-    const [rowData, setRowData] = useState<IFilterData>(filterData);
+  const [rowData, setRowData] = useState<IFilterData>(filterData);
 
-    const handleUpdate = (item: IItem, which: "where" | "tag" | "isOrNot") => {
-        const updatedRowData = { ...rowData };
-        updatedRowData[which] = item.title;
-        setRowData(updatedRowData);
+  const handleUpdate = (item: IItem, which: "where" | "tag" | "isOrNot") => {
+    const updatedRowData = { ...rowData };
+    updatedRowData[which] = item.title;
+    setRowData(updatedRowData);
 
-        onUpdate(updatedRowData);
-    };
+    onUpdate(updatedRowData);
+  };
 
-    return (
-        <div className="flex justify-between items-center">
-            <div className="flex justify-start items-center gap-2.5">
-                <span>تسک‌هایی که</span>
-                <Dropdown
-                    list={whereList}
-                    onClick={(item) => handleUpdate(item, "where")}
-                />
-                <span>آن‌ها</span>
-                <Dropdown
-                    list={tagList}
-                    onClick={(item) => handleUpdate(item, "tag")}
-                />
-                <Dropdown
-                    list={statusList}
-                    onClick={(item) => handleUpdate(item, "isOrNot")}
-                />
-            </div>
-            <button onClick={() => onDelete(filterData.id)}>
-                <DeleteIcon />
-            </button>
-        </div>
-    );
+  return (
+    <div className="flex justify-between items-center">
+      <div className="flex justify-start items-center gap-2.5">
+        <span>تسک‌هایی که</span>
+        <Dropdown
+          list={whereList}
+          onClick={(item) => handleUpdate(item, "where")}
+          defaultValue={filterData.where}
+        />
+        <span>آن‌ها</span>
+        <Dropdown
+          list={tagList}
+          onClick={(item) => handleUpdate(item, "tag")}
+          defaultValue={filterData.tag}
+        />
+        <Dropdown
+          list={statusList}
+          onClick={(item) => handleUpdate(item, "isOrNot")}
+          defaultValue={filterData.isOrNot}
+        />
+      </div>
+      <button onClick={() => onDelete(filterData.id)}>
+        <DeleteIcon />
+      </button>
+    </div>
+  );
 };
 
 export default FilterRow;
