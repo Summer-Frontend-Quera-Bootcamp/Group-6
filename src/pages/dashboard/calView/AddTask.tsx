a443df2ab3f490659385fa16b624b9c5ee3c8ef9
mahdi12ad <exxzam@gmail.com>
2023-10-26 11:26:54 +0330

    added calendar to project

--- src/pages/dashboard/calView/AddTask.tsx
+++ src/pages/dashboard/calView/AddTask.tsx
@@ -1,15 +1,105 @@
-export const AddTask: React.FC<{ theDate: string }> = ({
-    theDate,
-}): React.ReactNode => {
+import React, { useContext, useState } from "react";
+import { useTasksMutation } from "@services/Tasks/mutations/useTasksMutation";
+import { toast } from "react-toastify";
+import { ITasksRequest } from "@/types/api.types";
+import useQueryParams from "@/utils/useQueryParams";
+import { AppContext } from "@/context/store";
+import { Input, SubmitBtn } from "@/components/common";
+
+const PersianMonthNames: { [monthNumber: number]: string } = {
+    1: "فروردین",
+    2: "اردیبهشت",
+    3: "خرداد",
+    4: "تیر",
+    5: "مرداد",
+    6: "شهریور",
+    7: "مهر",
+    8: "آبان",
+    9: "آذر",
+    10: "دی",
+    11: "بهمن",
+    12: "اسفند",
+};
+
+const AddTask: React.FC<{ x: number; y: number; date: any }> = ({
+    x,
+    y,
+    date,
+}) => {
+    const [taskName, setTaskName] = useState("");
+    const taskMutation = useTasksMutation();
+    const { space, project } = useQueryParams();
+    const { state } = useContext(AppContext);
+
+    const projects = state.user.workspaces
+        .filter(({ id }) => space === undefined || id === Number(space))
+        .flatMap(
+            ({ projects, id: idx }) =>
+                projects?.map(({ id, name }) => ({ id, name, idx })) || []
+        );
+
+    const handleSubmit = async () => {
+        if (!space || !project) {
+            toast.error("لطفا پروژه مورد نظر خود را انتخاب کنید");
+            return;
+        } else if (taskName.trim() === "") {
+            toast.error("لطفا نام مورد نظر خود را وارد کنید");
+            return;
+        }
+
+        const defaultProject = projects?.find(
+            (p) => p && p?.id === Number(project)
+        );
+
+        let taskData: ITasksRequest = {
+            name: taskName,
+            deadline: String(date._i),
+            project: defaultProject,
+        };
+
+        toast.dismiss();
+        toast.loading("در حال افزودن");
+
+        taskMutation.mutate(taskData, {
+            onSuccess: async (e) => {
+                toast.dismiss();
+                toast.success("تسک با موفقیت افزوده شد.");
+
+                console.log(e, taskData);
+            },
+            onError: (error) => {
+                console.error(error);
+                toast.error("هنگام افزودن تسک، مشکلی پیش آمده.");
+            },
+        });
+    };
+
+    const monthNumber = date.jMonth() + 1;
+    const persianMonthName = PersianMonthNames[monthNumber];
+    const dateFormatted = `${date.jDate()} ${persianMonthName}`;
+
     return (
-        <div className="modal newtaskModal">
-            <input
-                placeholder="نام تسک را وارد کنید "
+        <div
+            className="newtaskModal w-full"
+            style={{ position: "fixed", top: y, left: x }}
+        >
+            <Input
+                placeholder="نام تسک را وارد کنید"
                 type="text"
-                className="theInput"
-            ></input>
-            <button className="theButton">ساخت تسک </button>
-            <p className="theP">{`${theDate}`}</p>
+                classNames="border-none focus:outline-none"
+                defaultValue={taskName}
+                onChange={(e) => setTaskName(e.target.value)}
+            />
+            <div className="flex justify-between w-full">
+                <p className="theP">{dateFormatted}</p>
+                <SubmitBtn
+                    value={"ساخت تسک"}
+                    className="calendarBtn"
+                    onSubmit={() => handleSubmit()}
+                />
+            </div>
         </div>
     );
 };
+
+export default AddTask;
